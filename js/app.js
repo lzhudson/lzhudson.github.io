4da3ee2f0909dabdea7b5e45245da957d23529df
Hudson Holanda <hudsonholanda55@gmail.com>
2019-10-24 11:40:19 -0300

    Adicionando javascript ao portfolio.

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const linksNav = document.querySelectorAll('.sidebar nav ul li');
+linksNav.forEach(function(link){
+  link.addEventListener('click', showAlert);
+});
+function showAlert(e){
+  let textLink = e.target.textContent;
+  alert(`${textLink} em desenvolvimento!`);
+}
